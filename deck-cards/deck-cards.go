e7d4cc4d3b85a0ed0e58f094ffe0dc5faace4dbe
Thiago Galvani <thiagopaiva99@gmail.com>
2024-10-24 08:28:50 -0300

    refactor: working with functions and value assignments

--- deck-cards/deck-cards.go
+++ deck-cards/deck-cards.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
+	card := newCard()
 	// card = "Five of Diamonds"
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
