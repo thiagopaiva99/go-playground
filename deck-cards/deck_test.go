4a4b3c6d75d2ee29a3343f8ce262cde7cae8414f
Thiago Galvani <thiagopaiva99@gmail.com>
2024-10-29 12:56:24 -0300

    test: adding tests for the createDeck function

--- deck-cards/deck_test.go
+++ deck-cards/deck_test.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	deck := createDeck()
+
+	if len(deck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(deck))
+	}
+}
