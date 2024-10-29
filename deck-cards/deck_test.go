0c3ad4a9f10a0b40ac0c21fbf1298dff9554f44d
Thiago Galvani <thiagopaiva99@gmail.com>
2024-10-29 12:59:11 -0300

    test: adding more assertions to the create deck function

--- deck-cards/deck_test.go
+++ deck-cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(deck) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(deck))
 	}
+
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", deck[0])
+	}
+
+	if deck[len(deck)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", deck[len(deck)-1])
+	}
 }
