412ff04cb0218fa9d83b371279eff512f18b2aab
Thiago Galvani <thiagopaiva99@gmail.com>
2024-10-30 12:53:57 -0300

    feat: creating the user struct value

--- structs/structs.go
+++ structs/structs.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-
+	user := person{firstName: "Thiago", lastName: "Galvani"}
+	fmt.Println(user)
 }
