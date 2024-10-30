6b0e6c4202300c86d4db1f4fbbbaf65274bb33f8
Thiago Galvani <thiagopaiva99@gmail.com>
2024-10-30 13:06:02 -0300

    feat: creating a function to update name handling with pointers

--- structs/structs.go
+++ structs/structs.go
@@ -18,6 +18,10 @@ func (p person) print() {
 	fmt.Println()
 }
 
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
 func main() {
 	user1 := person{firstName: "Thiago", lastName: "Galvani", contact: contactInfo{email: "thiago@example.com", zipCode: 12345}}
 	user1.print()
@@ -27,5 +31,9 @@ func main() {
 	user2.lastName = "Doe"
 	user2.contact.email = "john@example.com"
 	user2.contact.zipCode = 67890
+	user2.print()
+
+	user2Pointer := &user2
+	user2Pointer.updateName("Jimmy")
 	user2.print()
 }
